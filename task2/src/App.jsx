d6286e572b4d6a4b450f4c33cb404c6822ddc6ee
n530autique <darkness2241@list.ru>
2024-04-12 23:58:50 +0500

    task2

---
+++ task2/src/App.jsx
@@ -0,0 +1,31 @@
+import image1 from './Images/1.png'
+import './App.css'
+import { Card } from './сomponents/Card'
+
+function App() {
+
+  return (
+    <>
+      <Card image={image1}>Егор KAverin Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, quidem.</Card>
+      <Card image={image1}>python</Card>
+      <Card image={image1}>html + css</Card>
+      <Card image={image1}>гуглить</Card>
+      <Card image={image1}>js</Card>
+      <Card image={image1}>плохо пишу на реакте</Card>
+      <Card image={image1}>работать с докером</Card>
+      <Card image={image1}>node js</Card>
+      <Card image={image1}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, impedit praesentium?</Card>
+      <Card image={image1}>Lorem ipsum dolor sit.</Card>
+      <Card image={image1}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit eveniet odio corporis dolore possimus animi voluptatum, quam illo quas.</Card>
+      <Card image={image1}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla assumenda suscipit sed quidem voluptates porro, vero sint quas magni enim vitae aspernatur, molestiae dicta!</Card>
+      <Card image={image1}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis esse odio, consequuntur vitae illo nobis aliquam similique atque!</Card>
+      <Card image={image1}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae cupiditate, assumenda quidem voluptatibus sed ipsam repellendus quaerat dolore alias? Enim quas voluptatem dolores est nisi excepturi debitis, deserunt ratione eum! Ipsa laboriosam ratione quidem molestias corporis illo in, autem commodi corrupti deleniti illum reprehenderit unde harum expedita ad est adipisci odio similique voluptates, at pariatur obcaecati. Harum voluptatibus suscipit nostrum quasi nobis totam odio earum laborum ab? Odio modi magnam perspiciatis dolore impedit distinctio, adipisci ratione accusamus et dolorum perferendis quis fugit suscipit. Illum.</Card>
+      <Card image={image1}>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</Card>
+      <Card image={image1}>Lorem ipsum dolor sit amet consectetur adipisicing.</Card>
+
+      
+    </>
+  )
+}
+
+export default App
